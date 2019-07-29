d6fcf3096eda6aedc229da84995b4be5e3394218
Pablo Biedma <P.BiedmaNunez@student.tudelft.nl>
2019-07-29 20:26:31 +0200

    clicking on comment button but not commenting

--- InstagramBot.py
+++ InstagramBot.py
@@ -2,18 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import tkinter as tk
 from tkinter import simpledialog
-from pynput.keyboard import Key, Controller
+# from pynput.keyboard import Key, Controller
 import time
 import random
 
 # author: Pablo Biedma ig: pablo_biedma
 
+
 class InstagramBot:   # todo add functionality , then develop app
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, comment):
         self.username = username
         self.password = password
         self.driver = webdriver.Firefox()
+        self.comment = comment
 
     def close_browser(self):
         self.driver.close()
@@ -60,15 +62,17 @@ class InstagramBot:   # todo add functionality , then develop app
             # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             try:
                 time.sleep(random.randint(2, 4))
-                like_button = lambda: driver.find_element_by_xpath('//span[@aria-label="Like"]').click()
-                keyboard = Controller()
-                comment_button = lambda: driver.find_element_by_xpath('//span[@aria-label="Comment"]').click()
-                post_button = lambda: driver.find_element_by_class_name("_0mzm- sqdOP yWX7d        ").click()
+                like_button = driver.find_element_by_xpath('//span[@aria-label="Like"]').click()
+                comment_button = driver.find_element_by_xpath('//span[@aria-label="Comment"]').click()
+                text_area = driver.find_element_by_id("textarea")
+                post_button = driver.find_element_by_class_name("_0mzm- sqdOP yWX7d        ")
+                # liking photo
                 like_button().click()
                 time.sleep(0.25)
                 comment_button().click()
-                time.sleep(0.25)
-                keyboard.type("Amazing")
+                # typing comment
+                text_area.clear()
+                text_area.send_keys("hello world")
                 time.sleep(0.25)
                 post_button().click()
                 time.sleep(0.25)
@@ -86,14 +90,14 @@ if __name__ == "__main__":
                                       prompt="Please enter your instagram username: ")
     password = simpledialog.askstring(title="Password",
                                       prompt="Please enter your password: ")
-
+    comment = "Amazingg"
     print("\nLoading Bot...")
 
-    ig = InstagramBot(username, password)
+    ig = InstagramBot(username, password, comment)
     ig.login()
 
-    hashtags = ['fitness', 'mindfulness', 'adventure', 'photography', 'nofilter','seekdiscomfort',
-                'motivation', 'billywilder', 'trip', 'wanderlust', 'travel', 'travelling', 'discomfort', 'yestheory',
+    hashtags = ['fitness', 'mindfulness', 'adventure', 'photography', 'nofilter','seekdiscomfort', 'business', 'ceo', 'fcb', 'chess',
+                'motivation', 'billywilder', 'trip', 'wanderlust', 'travel', 'travelling', 'discomfort', 'yestheory', 'football', 'soccer',
                 'cinema', 'movies', 'technology', 'programming', 'deeplearning', 'ai', 'machinelearning', 'datascience', 'science', 'nba', 'basketball', 'okc',
                 'gym', 'sports', 'music', 'guitar', 'waterpolo', 'athlete', 'ted','tedtalk','inspiration','workout','cybersecurity', 'data', 'math', 'computer', 'computerscience',
                 'followforfollowback', 'follow4followback', 'like4follow', 'followbackinstantly', 'like4likes', 'likeforlikes', 'likeforlikeback', 'likeforfollow','followforlike', 'follow4like','likesforlike', 'likes4like']
@@ -105,7 +109,7 @@ if __name__ == "__main__":
             ig.like_photo(tag)
 
         except Exception:
-            ig.closeBrowser()
+            # ig.closeBrowser()
             time.sleep(60)
-            ig = InstagramBot(username, password)
+            ig = InstagramBot(username, password, comment)
             ig.login()
